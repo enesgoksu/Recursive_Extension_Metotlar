179002500572fcd3fa333ece86fb3b332ff389ff
Enes Goksu <enes-goksu.fb@hotmail.com>
2023-03-23 14:14:40 +0300

    program.cs değiştirildi

--- Program.cs
+++ Program.cs
@@ -4,16 +4,36 @@
     {
         static void Main(string[] args)
         {
-            // Recursive - Öz Yinemeli
+            // Recursive - Öz Yinelemeli
             // 3^4
 
             int result = 1;
             for (int i = 1; i < 5; i++)
                 result = result * 3;
                 Console.WriteLine(result);
-            {
-                
-            }
+
+            Islemler instance = new();
+            Console.WriteLine(instance.expo(3,4));
+
+            // Extension Metotlar
+
+            string ifade = "Enes Göksu";
+            bool sonuc = ifade.CheckSpaces();
+            Console.WriteLine(sonuc);
+            if (sonuc)
+                Console.WriteLine(ifade.RemoveWhiteSpaces());
+                Console.WriteLine(ifade.MakeUpperCase()); 
+                Console.WriteLine(ifade.MakeLowerCase());
+            
+            int[] dizi = {9,3,6,2,1,5,0};
+            dizi.SortArray();
+            dizi.EkranaYazdir();
+            int sayi = 5;
+            Console.WriteLine(sayi.IsEvenNumber());
+
+            Console.WriteLine(ifade.GetFirstCharacter());
+            
+            
         }
     }
 
@@ -25,5 +45,59 @@
             return sayi;
             return expo(sayi,üs-1)*sayi;
         }
+        // Expo(3,4)
+        //Expo(3,3) * 3;
+        //Expo(3,2) * 3 * 3;
+        //Expo(3,1) * 3 * 3 * 3;
+        // 3 * 3 * 3 * 3 = 3^4
+    }
+
+    public static class Extension{
+        public static bool CheckSpaces(this string param)
+        {
+            return param.Contains("Enes");     // İçinde Enes var mı
+        }
+        
+        public static string RemoveWhiteSpaces(this string param)
+        {
+            string[] dizi = param.Split(" ");  // " "' ları kaldır onların yerine - koy.
+            return string.Join("-",dizi);
+        }
+
+        public static string MakeUpperCase(this string param)
+        {
+            return param.ToUpper();   // Bütün hepsini büyük yazdır
+        }
+
+        public static string MakeLowerCase(this string param)
+        {
+            return param.ToLower();    // Bütün hepsini küçük yazdır
+        }
+
+        public static int[] SortArray(this int[] param)
+        {
+            Array.Sort(param);
+            return param;
+        }
+
+        public static void EkranaYazdir(this int[] param)
+        {
+            foreach (int item in param)
+            Console.WriteLine(item);
+                
+            
+        }
+
+        public static bool IsEvenNumber(this int param)
+        {
+            return param % 2 == 0;
+        }
+
+        public static string GetFirstCharacter(this string param)
+        {
+            return param.Substring(0,1);
+        }
+
+
     }
 }
